8d54926f76f5b10deefac73d578a33b4c0e5ed05
Ryo Yanagida <ry6@st-andrews.ac.uk>
2020-09-27 21:34:35 +0100

    modifying location list

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,14 @@
 export const locations = [
     {id: 'main_library', name: 'main library'},
+    {id: 'north_street', name: 'north street'},
     {id: 'market_street', name: 'market street'},
+    {id: 'south_street', name: 'south street'},
     {id: 'union', name: 'union'},
-    {id: 'dra', name: 'dra'},
-    {id: 'abh', name: 'abh'},
+    {id: 'dra', name: 'dra & fife park'},
+    {id: 'abh', name: 'abh & powell'},
     {id: 'andrew_meville', name: 'andrew meville'},
-    {id: 'uni_hall', name: 'uni hall'},
+    {id: 'uni_hall', name: 'uni hall & whitehorn'},
+    {id: 'bad_lands', name: 'bad lands'}
 ];
 
 export const locationsAndWelcome = [
