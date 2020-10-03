8181a18bcbf8ddd91a015b0432b01d084368ce1f
Ryo Yanagida <ry6@st-andrews.ac.uk>
2020-10-03 21:57:10 +0100

    updated locations

--- src/constants.js
+++ src/constants.js
@@ -3,18 +3,20 @@ export const locations = [
     {id: 'north_street', name: 'north street'},
     {id: 'market_street', name: 'market street'},
     {id: 'south_street', name: 'south street'},
+    {id: 'north_haugh', name: 'north haugh'},
     {id: 'union', name: 'union'},
     {id: 'dra', name: 'dra & fife park'},
     {id: 'abh', name: 'abh & powell'},
-    {id: 'andrew_meville', name: 'andrew meville'},
+    {id: 'andrew_melville', name: 'andrew melville'},
     {id: 'uni_hall', name: 'uni hall & whitehorn'},
-    {id: 'jbh', name: 'john burnet hall & annexe'},
-    {id: 'mcintosh', name: 'macintosh hall'},
-    {id: 'st_regulus', name: 'st regulus hall'},
-    {id: 'st_salvators', name: 'st salvator\'s'},
-    {id: 'gannochy', name: 'gannochy house'},
+    {id: 'jbh', name: 'john burnet'},
+    {id: 'mcintosh', name: 'macintosh'},
+    {id: 'st_regulus', name: 'st regulus'},
+    {id: 'st_salvators', name: 'sally\'s & gannochy'},
     {id: 'deans_court', name: 'deans court'},
-    {id: 'bad_lands', name: 'bad lands / outskirts'}
+    {id: 'ayton', name: 'ayton house'},
+    {id: 'east_shore', name: 'east shore'},
+    {id: 'outskirts', name: 'outskirts (badlands)'}
 ];
 
 export const locationsAndWelcome = [
