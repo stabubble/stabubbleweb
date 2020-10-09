6d1d42e7b7e6ab6448d2b5184a2ba749d3699c68
xz32 <xz32@users.noreply.github.com>
2020-10-09 12:01:42 +0100

    Emphasis on important words

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -24,7 +24,7 @@ function RegisterPage() {
                 <div style={{paddingTop: 10, width: '100%'}}>
                     <Card>write down, copy the passphrase or screenshot this screen</Card>
                     <Card>you will need it to login</Card>
-                    <Card>you won't see it again!</Card>
+                    <Card>you <strong>won't</strong> see it again!</Card>
                 </div>
                 <div style={{paddingTop: 10, width: '100%'}}>
                     <Card style={{backgroundColor: '#ffb347'}}>
@@ -35,9 +35,10 @@ function RegisterPage() {
                     </Card>
                 </div>
                 <div style={{paddingTop: 10, width: '100%'}}>
-                    <Card>don't share this passphrase with anyone else!</Card>
+                    <Card><strong>don't</strong> share this passphrase with anyone else!</Card>
                     <Card>otherwise, they will be able to see your activity</Card>
-                    <Card>this is not the code to invite a friend, you can find that at: me -> settings -> invite</Card>
+                    <Card>this is <strong>not</strong> the code to invite a friend, you can find that at:
+                        me -> settings -> invite</Card>
                 </div>
                 <Button modifier="large" style={{marginTop: 10, marginBottom: 10}}
                         onClick={() => history.replace('/')}>i have saved it!</Button>
